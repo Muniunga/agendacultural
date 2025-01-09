d72342063dd325164d46cdc352dbcc24dcadf585
Muniunga <muniunga2000@gmail.com>
2025-01-09 19:34:13 +0100

    Adicionar controlador de categorias com métodos para CRUD e rotas API correspondentes

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CategoriaModel;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    public function index()
+    {
+        $categorias = CategoriaModel::all();
+        return response()->json($categorias);
+    }
+
+    public function create()
+    {
+        return view('categoria.create');
+    }
+
+    public function store(Request $request)
+    {
+        $categoria = new CategoriaModel();
+        $categoria->nome = $request->nome;
+        $categoria->save();
+        return response()->json($categoria);
+    }
+
+    public function edit($id)
+    {
+        $categoria = CategoriaModel::find($id);
+        return view('categoria.edit', compact('categoria'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $categoria = CategoriaModel::find($id);
+        if (!$categoria) {
+            return response()->json(['message' => 'Categoria não encontrada'], 404);
+        }
+        $categoria->nome = $request->nome;
+        $categoria->save();
+        return response()->json($categoria);
+    }
+
+    public function destroy($id)
+    {
+        $categoria = CategoriaModel::find($id);
+        $categoria->delete();
+        return response()->json(['message' => 'Categoria eliminada com sucesso'], 404);
+    }
 }
