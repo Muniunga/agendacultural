54cbd424c4b11a991934e205fa430d36f6174113
Muniunga <muniunga2000@gmail.com>
2025-01-09 20:01:57 +0100

    Adicionar campos de descrição, imagem e rede social à tabela de eventos

--- database/migrations/2025_01_09_174333_create_eventos_table.php
+++ database/migrations/2025_01_09_174333_create_eventos_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('eventos', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
+            $table->string('descricao');
+            $table->string('imagem');
+            $table->string('rede_social');
             $table->string('local_evento');
             $table->date('data_evento');
             $table->time('hora_evento');
